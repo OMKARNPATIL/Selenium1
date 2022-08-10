f6cf17faa8a65df257aabc4537e56884dcc961d5
omkarnpatil <72076408+OMKARNPATIL@users.noreply.github.com>
2022-08-10 12:15:31 +0530

    Update omkar.java
    chnage done by team lead

--- src/omkar.java
+++ src/omkar.java
@@ -1,6 +1,7 @@
 
 public class omkar {
-				public static void main(String[] args) {
-					System.out.println("omkar");
-				}
+	public static void main(String[] args)
+	{
+	System.out.println("omkar");
+	}
 }
